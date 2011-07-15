69eef1cfcf12639f5ee797ec8d9433965668c51e
Kevin Saliou <kevin@b2deal.com>
2011-07-15 16:49:20 +0200

    move helpers + bootstrap to src dir

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-require_once __DIR__.'/../silex.phar';
-require_once __DIR__.'/../helpers.php';
+require_once __DIR__.'/../vendor/silex.phar';
+require_once __DIR__.'/helpers.php';
 
 use Symfony\Component\HttpFoundation\Response;
 
