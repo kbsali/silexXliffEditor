0da4842a416e4a9d68c720bc22364c9c1b616d96
Kevin Saliou <kevin@b2deal.com>
2011-07-15 16:36:41 +0200

    refactor : move index.php to bootstrap.php + app.php

---
+++ src/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+require_once __DIR__.'/../silex.phar';
+require_once __DIR__.'/../helpers.php';
+
+use Symfony\Component\HttpFoundation\Response;
+
+$app = new Silex\Application();
+
+$app->register(new Silex\Extension\TwigExtension(), array(
+    'twig.path'       => __DIR__.'/../views',
+    'twig.class_path' => __DIR__.'/../vendor/twig/lib',
+));
+return $app;
